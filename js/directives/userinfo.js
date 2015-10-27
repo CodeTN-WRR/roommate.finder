58ec1d52d3703c5476e93cf2777eca7bd3e767d3
Rob Scott <rob@eventbooking.com>
2015-10-27 16:46:42 -0400

    Fixes

--- js/directives/userinfo.js
+++ js/directives/userinfo.js
@@ -4,11 +4,6 @@ app.directive("userinfo", function() {
 		scope: {
 			idnum: "="
 		},
-<<<<<<< HEAD
-		templateUrl: "js/views/userinfo.html"
-	};
-=======
 		templateUrl: "js/directives/userinfo.html"
-	       };
->>>>>>> d29ad91a0e2467f4dc8e498a234e89230ad4d217
+	};
 });
