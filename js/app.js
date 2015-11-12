1fe83a6fd52f3ce5c708e2027e3fe412581a052b
Ayden Case <aydencase451@gmail.com>
2015-11-12 17:15:41 -0500

    Friends page

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ app.config(function($routeProvider) {
 			templateUrl: "js/views/profile-template.html"
 		})
 		.when("/friends_list", {
-			controller: "UserController",
+			controller: "SearchController",
 			templateUrl: "js/views/searchresults-template.html"
 		})
 		.when("/search_roommate", {
-			controller: "UserController",
+			controller: "SearchController",
 			templateUrl: "js/views/searchresults-template.html"
 		})
 		.otherwise({
